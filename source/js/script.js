8c10e29ec613e2fdf24cc0c8843a659bbec1fcee
test-sempt <semption@yandex.ru>
2021-01-07 00:14:31 +0300

    Добавил градиент на слайдере десктопа главной

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 // Slider
+let example = document.querySelector(".example");
 let slider = document.querySelector(".slider");
 let currentSlider = document.querySelector(".slider__item");
 let beforeSlide = document.querySelector(".slider__item--before");
@@ -7,12 +8,15 @@ let sliderBar = document.querySelector(".slider__bar");
 let sliderToggle = document.querySelector(".slider__toggle");
 let beforeButton = document.querySelector(".slider__button--before");
 let afterButton = document.querySelector(".slider__button--after");
+example.classList.add("example--before");
 
 if (slider) {
   afterButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     beforeSlide.classList.remove("slider__item--current");
     afterSlide.classList.add("slider__item--current");
+    example.classList.remove("example--before");
+    example.classList.add("example--after");
     sliderBar.style.margin = "3px 4px 0 43px";
     sliderToggle.style.left = "95%";
   })
@@ -21,6 +25,8 @@ if (slider) {
     evt.preventDefault();
     afterSlide.classList.remove("slider__item--current");
     beforeSlide.classList.add("slider__item--current");
+    example.classList.remove("example--after");
+    example.classList.add("example--before");
     sliderBar.style.margin = "3px 43px 0 4px";
     sliderToggle.style.left = "0%";
   })
